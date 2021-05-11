91c828b008c18be8ed33e3c12e017095618c343e
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-05-11 09:49:16 +0900

    주사위 두번던지기

--- 보드게임.c
+++ 보드게임.c
@@ -28,6 +28,18 @@ void main(void)
 
 	printf("현재위치 : %d\n\n", position);
 
+	/* 첫번째 던짐 */
+	// 주사위는 1 ~ 6까지 나오게 설정
+	dice = rand() % 6 + 1;
+	printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
+
+	// 던진 주사위값을 현재 위치에 적용
+	position += dice;
+	printf("현재위치 : %d\n\n", position);
+
+
+
+	/* 두번째 던짐 */
 	// 주사위는 1 ~ 6까지 나오게 설정
 	dice = rand() % 6 + 1;
 	printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
