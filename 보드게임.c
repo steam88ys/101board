8242cbf24bd732d74aaf9275726ac1fe2070b439
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-05-11 09:59:43 +0900

    주사위를 무한반복

--- 보드게임.c
+++ 보드게임.c
@@ -28,24 +28,19 @@ void main(void)
 
 	printf("현재위치 : %d\n\n", position);
 
-	/* 첫번째 던짐 */
-	// 주사위는 1 ~ 6까지 나오게 설정
-	dice = rand() % 6 + 1;
-	printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
+	// 무한반복
+	while (1) 
+	{
+		/* 던짐 */
+		// 주사위는 1 ~ 6까지 나오게 설정
+		dice = rand() % 6 + 1;
+		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
 
-	// 던진 주사위값을 현재 위치에 적용
-	position += dice;
-	printf("현재위치 : %d\n\n", position);
-	getchar();
+		// 던진 주사위값을 현재 위치에 적용
+		position += dice;
+		printf("현재위치 : %d\n\n", position);
+		getchar();
+	}
+	
 
-
-	/* 두번째 던짐 */
-	// 주사위는 1 ~ 6까지 나오게 설정
-	dice = rand() % 6 + 1;
-	printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
-
-	// 던진 주사위값을 현재 위치에 적용
-	position += dice;
-	printf("현재위치 : %d\n\n", position);
-	getchar();
 }
