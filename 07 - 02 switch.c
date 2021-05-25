2789552da9477f1a9d7f4f1ab7cd8a04f3b20473
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-05-25 09:11:31 +0900

    if문을 switch로 바꾸기 1 (아래코드가 계속 실행되는 문제가 있음)

--- 07 - 02 switch.c
+++ 07 - 02 switch.c
@@ -6,22 +6,28 @@ void main(void)
 	printf("숫자를 입력하세요(1~8까지) : ");
 	scanf("%d", &input);
 
-	if (input == 1)
+	switch(input) 
+	{
+		//if(input == 1)
+	case 1:
 		printf("피카츄\n");
-	else if (input == 2)
+		//else if(input == 2)
+	case 2: 
 		printf("라이츄\n");
-	else if (input == 3)
+	case 3:
 		printf("파이리\n");
-	else if (input == 4)
+	case 4:
 		printf("꼬부기\n");
-	else if (input == 5)
+	case 5: 
 		printf("버터풀\n");
-	else if (input == 6)
+	case 6: 
 		printf("야도란\n");
-	else if (input == 7)
+	case 7: 
 		printf("피죤투\n");
-	else if (input == 8)
+	case 8:
 		printf("또가스\n");
-	else
-		printf("잘못 입력하였습니다.");
+	default:
+		printf("잘못 입력하였습니다. \n");
+		//else
+	}
 }
