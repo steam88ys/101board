54af6540028065afc1b5ffc4524fb04dcec8cfee
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-06-15 10:00:56 +0900

    [문법연습] 구구단에 continue 추가

--- 구구단.c
+++ 구구단.c
@@ -6,13 +6,13 @@ void main(void)
 	{
 		if (dan == 4)
 		{
-			break;   //바깥쪽 for문에 대한 break (단은 3까지만)
+			continue;   //바깥쪽 for문에 대한 continue (4단만 skip)
 		}
 		for (int i = 1; i <= 9; i++)
 		{
 			if (i == 5)
 			{
-				break; //안쪽 for문에 대한 break  (수는 4까지만)
+				continue; //안쪽 for문에 대한 continue  (수5만 skip)
 			}
 			printf("%d X %d = %d \n", dan, i, dan * i);
 		}
