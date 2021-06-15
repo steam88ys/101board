419f89b57f4ec5a654f89badf89ed9e66bc79026
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-06-15 09:37:01 +0900

    [문법연습] 구구단 완성

--- 구구단.c
+++ 구구단.c
@@ -2,11 +2,31 @@
 
 void main(void)
 {
-	for (int dan = 2; dan <= 3; dan++)
+	for (int dan = 1; dan <= 9; dan++)
 	{
 		for (int i = 1; i <= 9; i++)
 		{
 			printf("%d X %d = %d \n", dan, i, dan * i);
 		}
+		printf("\n");
 	}
 }
+
+
+/*  (while문으로 바꾸기)
+	
+	int dan=1;
+	while(dan <=9)
+	{
+		int i=1;
+		while(i<=9)
+		{
+			printf("%d X %d = %d \n", dan, i, dan * i);
+			i++;
+		}
+		printf("\n");
+		dan++;
+	}
+
+
+*/
