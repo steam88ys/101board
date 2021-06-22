533da345b32a87cd37656effe28cc8f0e32a9088
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-06-22 10:08:02 +0900

    [문법연습] 함수의 구성요소

---
+++ 문법연습/08 - 01 함수의 구성요소.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+// 두 개의 숫자를 더해서 반환하는 함수
+int sum(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+void main(void) 
+{
+	int c;
+	c = sum(1, 2);
+	printf("%d", c);
+}
+
+// void일때 return 불가능
