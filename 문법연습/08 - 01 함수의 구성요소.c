6de8b50081311833d400b5de54d864302d23b2f3
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-06-22 10:20:05 +0900

    반환형 없는(void) 함수로 변환

--- 문법연습/08 - 01 함수의 구성요소.c
+++ 문법연습/08 - 01 함수의 구성요소.c
@@ -1,16 +1,14 @@
 #include <stdio.h> 
 
 // 두 개의 숫자를 더해서 반환하는 함수
-int sum(int num1, int num2)
+void sum(int num1, int num2)
 {
-	return num1 + num2;
+	printf("%d\n", num1 + num2);
 }
 
 void main(void) 
 {
-	int c;
-	c = sum(1, 2);
-	printf("%d", c);
+	sum(1, 2);
 }
 
 // void일때 return 불가능
