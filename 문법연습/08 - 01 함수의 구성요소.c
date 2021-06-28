05d1bd1fd2d3c0f0665a65e64789d875b8dc8cd9
steam88ys <82978463+steam88ys@users.noreply.github.com>
2021-06-28 13:53:18 +0900

    함수의 반환형 변환

--- 문법연습/08 - 01 함수의 구성요소.c
+++ 문법연습/08 - 01 함수의 구성요소.c
@@ -1,14 +1,16 @@
 #include <stdio.h> 
 
-// 두 개의 숫자를 더해서 반환하는 함수
-void sum(int num1, int num2)
+ // 두 개의 숫자를 더해서 반환하는 함수
+ //void -> 반환형이 없다 -> return 이 없다
+double sum(int num1, int num2)   
 {
-	printf("%d\n", num1 + num2);
+	return (double)(num1 + num2) + 0.14;
 }
 
 void main(void) 
 {
-	sum(1, 2);
+	double a = sum(1, 2);
+	printf("%lf", a);
 }
 
 // void일때 return 불가능
