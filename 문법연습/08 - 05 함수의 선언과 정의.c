dda94edc179b6f17830d27b7f5f2394fc327bfef
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-08-23 14:04:35 +0900

    함수를 먼저 선언하여 정의할 때의 충돌을 방지함

--- 문법연습/08 - 05 함수의 선언과 정의.c
+++ 문법연습/08 - 05 함수의 선언과 정의.c
@@ -1,9 +1,22 @@
 ﻿#include <stdio.h>
 
+// 함수의 선언
+int sum(int a, int b);
+int sum2(int a, int b);
+int sum3(int a, int b);
+int sum4(int a, int b);
+
+void main(void)
+{
+	int c = sum(1, 2);
+	printf("%d", c);
+}
+
+
 // 함수의 정의(definition)
 int sum(int a, int b)
 {
-	// 에러 sum2가 정의되기 전에 sum2를 호출함
+	// !!에러 sum2가 정의되기 전에 sum2를 호출함
 	return sum2(a, b);
 }
 
@@ -20,10 +33,4 @@ int sum3(int a, int b)
 int sum4(int a, int b)
 {
 	return a + b;
-}
-
-void main(void)
-{
-	int c=sum(1, 2);
-	printf("%d", c);
 }
