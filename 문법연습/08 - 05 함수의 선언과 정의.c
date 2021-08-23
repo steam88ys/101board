af042b84a16dca8868195f115cb191d3c8dcef26
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-08-23 14:00:21 +0900

    함수를 정의하면서 충돌(정의되어 있지 않은 함수를 먼저 호출하려고 함)

--- 문법연습/08 - 05 함수의 선언과 정의.c
+++ 문법연습/08 - 05 함수의 선언과 정의.c
@@ -3,6 +3,22 @@
 // 함수의 정의(definition)
 int sum(int a, int b)
 {
+	// 에러 sum2가 정의되기 전에 sum2를 호출함
+	return sum2(a, b);
+}
+
+int sum2(int a, int b)
+{
+	return sum3(a, b);
+}
+
+int sum3(int a, int b)
+{
+	return a + b;
+}
+
+int sum4(int a, int b)
+{
 	return a + b;
 }
 
