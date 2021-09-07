f5946074ed1ab810aaed2ac99236cea7baa2a361
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-09-07 09:13:24 +0900

    반복문을 효육적으로

---
+++ 10 - 03 문자열 예제2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	// NULL문자('\0')가 포함되어서 12개의 공간을 할당
+	char str[] = "Hello World";	// char 1byte
+	printf("%d\n", sizeof(str));
+
+	// 문자열의 갯수를 세지 않고 NULL문자 여부만을 확인
+	for (int i = 0; str[i] != '\0' ; i++)
+		printf("%c", str[i]);
+}
