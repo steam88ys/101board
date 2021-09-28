e0822cb4b2e56c56d2e113adcf2a1f6989cf190f
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-09-28 12:07:34 +0900

    Call By Value에서 Call By Pointer로 바꾸기

--- 11 - 05 지역변수 바꾸기.c
+++ 11 - 05 지역변수 바꾸기.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-void changeA(int num);
+void changeA(int* num);
 
 void main(void)
 {
 	int A = 3;
-	printf("바꾸기 전 %d\n", A);
+	printf("바꾸기 전 %d \n", A);
 	// 함수의 argument로 A의 주소값을 넘김
-	// 변수 A 자체를 넘기는것이 아니다.
-	changeA(A);
-	printf("바꾸기 후 %d\n", A);
+	// 매개변수 num은 변수 A를 가리킨다
+	changeA(&A);
+	printf("바꾸기 후 %d \n", A);
 }
 
-void changeA(int num)
+void changeA(int* num)
 {
-	num = 5;
+	// num이 가리키고 있는 변수(A) 안에 들어있는 값을 5로 바꾼다
+	*num = 5;
 }
