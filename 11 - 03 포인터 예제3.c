2b686df94db9315115632e50003182cd8a8e8cb4
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-09-28 10:55:40 +0900

    가리키는 대상을 바꿔봄

---
+++ 11 - 03 포인터 예제3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int num1 = 3, num2 = 30;
+	printf("num1 안에 들어있는 값 %d\n", num1);
+	printf("num2 안에 들어있는 값 %d\n\n", num2);
+
+	// ptr이 num1을 가리킴 (int* ptr = &num1;)
+	int* ptr;
+	ptr = &num1;
+
+	// num1 값이 바뀜
+	*ptr = 4;	
+
+	// ptr이 num2를 가리킴
+	ptr = &num2;	
+
+	// num2 값이 바뀜
+	*ptr = 40;	
+	printf("num1 안에 들어있는 값 %d\n", num1);		// 4
+	printf("num2 안에 들어있는 값 %d\n\n", num2);		// 40
+}
