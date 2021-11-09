951724324af17803b709dd938699df5c66c6ab8a
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-11-09 10:21:35 +0900

    로또문제 (break 줄여야 함)

---
+++ 타율적숙제02.c
@@ -0,0 +1,34 @@
+/*
+	타율적숙제02
+	정렬된 로또 당첨번호 answer_number
+	정렬된 기입번호 my_number
+	confirm_1st : 당첨인지 아닌지를 확인하는 함수
+*/
+
+#include <stdio.h>
+
+int confirm_1st(void);
+
+void main(void)
+{
+	
+	// 로또번호(오름차순 정렬)
+	int answer_number[6] = { 2, 3, 8, 11, 15, 41 };
+	// 자신이 기입한 번호(오름차순 정렬)
+	int my_number[6] = { 2, 3, 8, 11, 15, 41  };
+	
+	if ( confirm_1st(answer_number, my_number) ) {
+		printf("1등에 당첨되었습니다. \n");
+	}
+}
+
+int confirm_1st(int answer[6], int my[6])
+{
+	int check_num = 0;
+	for (int i = 0; i < 6; i++) {
+		check_num = i;
+		if (answer[i] != my[i])	break;		// break 줄일 수 있음
+	}
+	if (check_num == 5) return 1;
+	else return 0;
+}
