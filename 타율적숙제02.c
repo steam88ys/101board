17bbe2a1d67007d6b159c3350b84881b0f6b9e37
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-11-16 10:16:41 +0900

    로또문제 수정

--- 타율적숙제02.c
+++ 타율적숙제02.c
@@ -15,19 +15,19 @@ void main(void)
 	// 로또번호(오름차순 정렬)
 	int answer_number[6] = { 2, 3, 8, 11, 15, 41 };
 	// 자신이 기입한 번호(오름차순 정렬)
-	int my_number[6] = { 2, 3, 8, 11, 15, 41  };
+	int my_number[6] = { 2, 3, 8, 11, 15, 40 };
 	
 	if ( confirm_1st(answer_number, my_number) ) {
 		printf("1등에 당첨되었습니다. \n");
 	}
 }
 
-int confirm_1st(int answer[6], int my[6])
+int confirm_1st(int* answer, int* my)	// int[] answer도 가능
 {
 	int check_num = 0;
 	for (int i = 0; i < 6; i++) {
+		if (answer[i] != my[i]) break;
 		check_num = i;
-		if (answer[i] != my[i])	break;		// break 줄일 수 있음
 	}
 	if (check_num == 5) return 1;
 	else return 0;
