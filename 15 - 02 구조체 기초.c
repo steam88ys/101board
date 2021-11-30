d3614fb1443ddb4f6fac351ca0b7380647c00ae0
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-11-30 09:39:31 +0900

    구조체 기초 typedef 사용

--- 15 - 02 구조체 기초.c
+++ 15 - 02 구조체 기초.c
@@ -1,6 +1,8 @@
 // printf 함수를 가지고 있음.
 #include <stdio.h>
 
+typedef struct point Point;
+
 struct point {
 	int x;
 	int y;
@@ -10,15 +12,15 @@ void main(void)
 {
 
 	// 선언과 동시에 초기화 p1.x는 10, p1.y는 20
-	struct point p1 = { 10, 20 };
+	Point p1 = { 10, 20 };
 
 	// point p2; // 에러발생 반드시 struct를 넣어야함
-	struct point p2;
+	Point p2;
 	p2.x = 20;
 	p2.y = 40;
 
 	printf("(%d, %d) \n", p1.x, p1.y);
 	printf("(%d, %d) \n", p2.x, p2.y);
-	printf("구조체의 크기는 %d", sizeof(struct point));
+	printf("구조체의 크기는 %d", sizeof(Point));
 
 }
