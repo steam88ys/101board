3ff334cb87cf223e72dc1f2c3444221adb45cb1f
yunseo Kim <82978463+steam88ys@users.noreply.github.com>
2021-12-07 11:19:51 +0900

    타율적숙제02_return부분 수정

--- 타율적숙제02.c
+++ 타율적숙제02.c
@@ -11,24 +11,22 @@ int confirm_1st(void);
 
 void main(void)
 {
-	
+
 	// 로또번호(오름차순 정렬)
 	int answer_number[6] = { 2, 3, 8, 11, 15, 41 };
 	// 자신이 기입한 번호(오름차순 정렬)
-	int my_number[6] = { 2, 3, 8, 11, 15, 40 };
-	
-	if ( confirm_1st(answer_number, my_number) ) {
+	int my_number[6] = { 2, 3, 8, 11, 15, 41 };
+
+	if (confirm_1st(answer_number, my_number)) {
 		printf("1등에 당첨되었습니다. \n");
 	}
 }
 
-int confirm_1st(int* answer, int* my)	// int[] answer도 가능
+int confirm_1st(int answer[6], int my[6])
 {
-	int check_num = 0;
 	for (int i = 0; i < 6; i++) {
-		if (answer[i] != my[i]) break;
-		check_num = i;
+		if (answer[i] != my[i])
+			return 0;
 	}
-	if (check_num == 5) return 1;
-	else return 0;
+	return 1;
 }
